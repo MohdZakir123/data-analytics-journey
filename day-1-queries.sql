624c20e687484ab01e2c49fe1c9fa93926b70417
MohdZakir123 <109594593+MohdZakir123@users.noreply.github.com>
2026-05-12 11:17:45 +0530

    mm

---
+++ day-1-queries.sql
@@ -0,0 +1,6 @@
+-- Day 1: SELECT and FROM basics
+-- Query 1: Show all columns from customers table
+SELECT * FROM customers;
+
+-- Query 2: Show only name and city
+SELECT name, city FROM customers;
