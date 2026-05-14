351c8ce83e7d6a347f0d456b6f6930c4f3b42666
MohdZakir123 <zakirmohd224@gmail.com>
2026-05-14 11:05:14 +0530

    Day 4: Learnt INNER JOIN, LEFT JOIN, GROUP BY with multiple tables. Ran 5 queries.

--- sql-practice/day-4-joins.sql
+++ sql-practice/day-4-joins.sql
@@ -11,3 +11,10 @@ INNER JOIN language l ON f.language_id = l.language_id
 GROUP BY l.name;
 LIMIT 10;
 
+-- 3. Customer details with there total rental amount (JOIN + SUM)
+SELECT c.customer_id, CONCAT(c.first_name, ' ', c.last_name) AS name, SUM(p.amount) AS total_spent
+FROM customer c
+INNER JOIN payment p ON c.customer_id = p.customer_id
+GROUP BY c.customer_id
+ORDER BY total_spent DESC
+LIMIT 10;
